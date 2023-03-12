99a6f1db6962f16cc50177d7574952f373658f87
theokoles <gabrieltrahan777@hotmail.com>
2023-03-12 15:13:49 -0500

    Sets up backend

---
+++ recycle_home/src/main/java/com/example/recycle_home/RecycleHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.recycle_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleHomeApplication.class, args);
+	}
+
+}
