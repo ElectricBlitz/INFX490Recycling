31d45fc04c9e56a6012e7b2699e2c9ebf3773c5b
Phillip Guidry <pguidry00@gmail.com>
2023-03-14 16:33:00 -0500

    More backend additions

---
+++ backend/src/test/java/com/example/INFX490Recycling/Infx490RecyclingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.INFX490Recycling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Infx490RecyclingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
