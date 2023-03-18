1ca1d6c5f651b430f2116ec1e0afa3077875b8ed
Phillip Guidry <pguidry00@gmail.com>
2023-03-18 15:43:48 -0500

    modifications to include map (currenly broken)

--- recycleHome/src/app/app.module.ts
+++ recycleHome/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
