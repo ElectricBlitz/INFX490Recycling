f916e04fcb7d039b155241f71a6b48a3f26024b8
ryneguercio <C00417866@louisiana.edu>
2023-03-21 15:45:40 -0500

    Rewards changes
    Edited the rewards screen to display user points, also added the Rewards class

--- recycleHome/src/app/rewards/rewards.component.ts
+++ recycleHome/src/app/rewards/rewards.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reward } from '../classes/reward';
 
 @Component({
   selector: 'app-rewards',
   templateUrl: './rewards.component.html',
   styleUrls: ['./rewards.component.scss']
 })
-export class RewardsComponent {
+export class RewardsComponent implements OnInit {
+    constructor(){
 
+    }
+
+    ngOnInit(): void {
+        
+    }
+
+    userPoints:number = 33576732;
 }
