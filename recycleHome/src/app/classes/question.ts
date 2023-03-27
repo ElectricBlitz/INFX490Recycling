b1ac4615709132731da3de81b1e2d664ad67d28e
ryneguercio <C00417866@louisiana.edu>
2023-03-27 11:26:56 -0500

    Question display
    Added code for the display of a random question

--- recycleHome/src/app/classes/question.ts
+++ recycleHome/src/app/classes/question.ts
@@ -1,17 +1,15 @@
 export class Question {
     id:number;
     text:string;
-    image:string;
     answer1:string;
     answer2:string;
     answer3:string;
     answer4:string;
     cAnswer:string;
 
-    constructor(id:number,text:string,image:string,answer1:string,answer2:string,answer3:string,answer4:string,cAnswer:string){
+    constructor(id:number,text:string,answer1:string,answer2:string,answer3:string,answer4:string,cAnswer:string){
         this.id = id;
         this.text = text;
-        this.image = image;
         this.answer1 = answer1;
         this.answer2 = answer2;
         this.answer3 = answer3;
--- recycleHome/src/app/quiz/quiz.component.ts
+++ recycleHome/src/app/quiz/quiz.component.ts
@@ -13,7 +13,17 @@ export class QuizComponent implements OnInit {
 
   }
 
-  questions:Question[] = [];
+  questions:Question[] = [new Question(0,"The decomposition time of batteries is roughly:","350 years","100 years","5 months","10 years","100 years"),
+                          new Question(1,"Which item can be collected by municipal recycling?","any plastics","soft plastics","clean and dry hard plastics","all of the above","clean and dry hard plastics")];
 
-  
+  getRandomQuestionID() {
+    return Math.floor(Math.random() * (Math.floor(this.questions.length) - Math.ceil(0)) + Math.ceil(0));
+  }
+
+  currentQuestion:number = this.getRandomQuestionID();
+
+  nextQuestion(){
+    this.currentQuestion = this.getRandomQuestionID();
+  }
+
 }
