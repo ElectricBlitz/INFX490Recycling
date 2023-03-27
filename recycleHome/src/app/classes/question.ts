13c053b22008f18b97afdb50a57e4278b198f69f
ryneguercio <C00417866@louisiana.edu>
2023-03-27 09:15:12 -0500

    Question Class
    Made the question class which will be used for the quiz functionality

---
+++ recycleHome/src/app/classes/question.ts
@@ -0,0 +1,21 @@
+export class Question {
+    id:number;
+    text:string;
+    image:string;
+    answer1:string;
+    answer2:string;
+    answer3:string;
+    answer4:string;
+    cAnswer:string;
+
+    constructor(id:number,text:string,image:string,answer1:string,answer2:string,answer3:string,answer4:string,cAnswer:string){
+        this.id = id;
+        this.text = text;
+        this.image = image;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.cAnswer = cAnswer;
+    }
+}
--- recycleHome/src/app/quiz/quiz.component.ts
+++ recycleHome/src/app/quiz/quiz.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Question } from '../classes/question';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void{
+
+  }
+
+  questions:Question[] = [];
+
+  
 }
