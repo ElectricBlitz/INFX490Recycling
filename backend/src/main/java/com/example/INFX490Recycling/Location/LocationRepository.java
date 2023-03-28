0f1efc673ac4624fb407d5d891593ea2190fc25d
Phillip Guidry <pguidry00@gmail.com>
2023-03-28 15:07:03 -0500

    updated imports

--- backend/src/main/java/com/example/INFX490Recycling/Location/LocationRepository.java
+++ backend/src/main/java/com/example/INFX490Recycling/Location/LocationRepository.java
@@ -1,9 +1,6 @@
 package com.example.INFX490Recycling.Location;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-// location cannot be resolve to a type
-// even though it's in the same package???????????
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
---
+++ backend/src/main/java/com/example/INFX490Recycling/Location/LocationService.java
@@ -0,0 +1,1 @@
+package com.example.INFX490Recycling.Location;
