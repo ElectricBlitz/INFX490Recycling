67b535d01235a47e2208d4367bc275d375381daa
Phillip Guidry <pguidry00@gmail.com>
2023-03-29 16:38:22 -0500

    Updated URLs

--- backend/src/main/java/com/example/backend/Location/LocationController.java
+++ backend/src/main/java/com/example/backend/Location/LocationController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("api/locations")
+@RequestMapping("locations")
 public class LocationController {
 
     @Autowired
     private LocationRepository locationRepository;
 
-    @GetMapping("api/locations")
+    @GetMapping("locations")
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
     }
