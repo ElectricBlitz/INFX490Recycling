c4afa9667a09c1b45bfde2e60771feab94ba644b
Dylan <c00047310@louisiana.edu>
2023-04-24 16:25:03 -0500

    Hashing done

--- backend/src/main/java/com/example/backend/Account/AccountController.java
+++ backend/src/main/java/com/example/backend/Account/AccountController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,9 +22,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
-
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
   @Autowired
   private AccountRepository AccountRepository;
@@ -53,7 +54,7 @@ public class AccountController {
     
     try{
       if(AccountRepository.findByUsername(username) != null){
-        if(AccountRepository.findByUsername(username).getPassword().equals(BCrypt.hashpw(password, BCrypt.gensalt()))){
+        if(BCrypt.checkpw(password, AccountRepository.findByUsername(username).getPassword())){
           return new ResponseEntity<>(true, HttpStatus.OK);
         } 
         else return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
--- backend/src/main/java/com/example/backend/Receipts/ReceiptsController.java
+++ backend/src/main/java/com/example/backend/Receipts/ReceiptsController.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReceiptsController {
     @Autowired
     private ReceiptsRepository ReceiptsRepository;
